b16aa42603a875854de108edd5ffbd83b4c622c0
MrGassner <110873341+MrGassner@users.noreply.github.com>
2023-03-02 19:30:57 -0300

    Add basic API functionality

--- index.js
+++ index.js
@@ -27,26 +27,33 @@ const weekDays = (() => {
     }
 })();
 
-const weatherInfo = (() => {
+function weatherAPI(selectedCity) {
     const city = document.querySelector('.city')
-    const state = document.querySelector('.state')
+    const country = document.querySelector('.country')
     const temperature = document.querySelector('.temperature')
     const description = document.querySelector('.description')
     const highest = document.querySelector('.highest')
     const lowest = document.querySelector('.lowest')
-    const input = document.querySelector('.searchInput')
-    const submitBtn = document.querySelector('button[type="submit"]')
-})();
+    const regionNamesInEnglish = new Intl.DisplayNames(['en'], { type: 'region' });
 
-function weatherAPI(selectedCity) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&APPID=75a0348f82edf39f41dec5c2d6cd790f`)
         .then(response => response.json())
         .then(response => {
+            city.innerHTML = response.name
+            country.innerHTML = regionNamesInEnglish.of(response.sys.country)
+            temperature.innerHTML = Math.round(response.main.temp - 273.15)
+            description.innerHTML = response.weather[0].description
+            highest.innerHTML = `H: ${Math.round(response.main.temp_max - 273.15)}°`
+            lowest.innerHTML = `L: ${Math.round(response.main.temp_min - 272.15)}°`
             console.log(response)
-        }) 
+        })
+
 }
 
 function cityHandler(city = null) {
+
+    console.log(city)
+
     if (city === null) {
         if (localStorage.city) {
             selectedCity = JSON.parse(localStorage.getItem('city'))
@@ -63,3 +70,10 @@ function cityHandler(city = null) {
     localStorage.city = JSON.stringify(selectedCity)
     return selectedCity
 }
+
+document.querySelector('button[type="submit"]').addEventListener('click', () => {
+    const input = document.querySelector('.searchInput')
+    cityHandler(input.value)
+})
+
+weatherAPI(cityHandler())
